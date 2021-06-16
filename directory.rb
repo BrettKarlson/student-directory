b69cf92c4e4eafe4a61b4b9bd144015fedc79beb
Brett Karlson <brettjkarlson@gmail.com>
2021-06-16 20:44:09 +0100

    refactored by extracting into new methods

--- directory.rb
+++ directory.rb
@@ -1,21 +1,19 @@
+@students = []
+
 # A method to allow user input of names
 def input_students
   puts "Please enter the names of the students:"
   puts "To finish, just hit return twice."
-  #create an empty array
-  students = []
   # get the first name.
   name = gets.chomp
   # until the name is empty, repeat this code. 'while' and ! replaced with 'until'
   until name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :june}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :june}
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
   end
-  # return the array of students
-  students
 end
 
 # method to print header.
@@ -25,8 +23,8 @@ def print_header
 end
 # method to iterate over the array and puts each item
 # change name from 'print' to 'print_names" (Ruby keyword)
-def print_names(students)
-  students.each do |student|
+def print_students_list
+  @students.each do |student|
   puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
@@ -35,30 +33,36 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  # show the students
+  print_header
+  print_students_list
+  print_footer(@students)
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit 
+  else
+    puts "I'm sorry, I don't know what you meant. Please try again"
+  end
+end
+
 def interactive_menu
-  loop do
-    students = []
-    # 1. Print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      # input the students
-      students = input_students
-    when "2"
-      # show the students
-      print_header
-      print_names(students)
-      print_footer(students)
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I'm sorry, I don't know what you meant. Please try again"
-    end
+  loop do 
+    print_menu
+    process(gets.chomp)
   end
 end
 # nothing happens until we call the methods
